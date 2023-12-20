c47e2dab49617ca13ba79eebce80e3964aaf2818
dhaval-l-eq <130955904+dhaval-l-eq@users.noreply.github.com>
2023-12-20 19:00:00 +0530

    project setup in progress

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,48 +10,48 @@ module.exports = {
             colors: {
                 primary: {
                     DEFAULT: '#4361ee',
-                    light: '#eaf1ff',
+                    100: '#eaf1ff',
                     'dark-light': 'rgba(67,97,238,.15)',
                 },
                 secondary: {
                     DEFAULT: '#805dca',
-                    light: '#ebe4f7',
+                    100: '#ebe4f7',
                     'dark-light': 'rgb(128 93 202 / 15%)',
                 },
                 success: {
                     DEFAULT: '#00ab55',
-                    light: '#ddf5f0',
+                    100: '#ddf5f0',
                     'dark-light': 'rgba(0,171,85,.15)',
                 },
                 danger: {
                     DEFAULT: '#e7515a',
-                    light: '#fff5f5',
+                    100: '#fff5f5',
                     'dark-light': 'rgba(231,81,90,.15)',
                 },
                 warning: {
                     DEFAULT: '#e2a03f',
-                    light: '#fff9ed',
+                    100: '#fff9ed',
                     'dark-light': 'rgba(226,160,63,.15)',
                 },
                 info: {
                     DEFAULT: '#2196f3',
-                    light: '#e7f7ff',
+                    100: '#e7f7ff',
                     'dark-light': 'rgba(33,150,243,.15)',
                 },
                 dark: {
                     DEFAULT: '#3b3f5c',
-                    light: '#eaeaec',
+                    100: '#eaeaec',
                     'dark-light': 'rgba(59,63,92,.15)',
                 },
                 black: {
                     DEFAULT: '#0e1726',
-                    light: '#e3e4eb',
+                    100: '#e3e4eb',
                     'dark-light': 'rgba(14,23,38,.15)',
                 },
                 white: {
                     DEFAULT: '#ffffff',
-                    light: '#e0e6ed',
-                    dark: '#888ea8',
+                    100: '#e0e6ed',
+                    800: '#888ea8',
                 },
             },
             fontFamily: {
