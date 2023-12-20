bfecd4d2e1fe930d8bf713a37721a8476d17bfef
dhaval-l-eq <130955904+dhaval-l-eq@users.noreply.github.com>
2023-12-20 15:32:41 +0530

    raw theme setup

---
+++ src/router/index.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import BlankLayout from "../layouts/BlankLayout";
+import DefaultLayout from "../layouts/DefaultLayout";
+import { routes } from "./routes";
+
+const finalRoutes = routes.map((route) => {
+   return {
+      ...route,
+      element:
+         route.layout === "blank" ? (
+            <BlankLayout>{route.element}</BlankLayout>
+         ) : (
+            <DefaultLayout>{route.element}</DefaultLayout>
+         ),
+   };
+});
+
+const router = createBrowserRouter(finalRoutes);
+
+export default router;
